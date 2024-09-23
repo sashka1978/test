efd3df3a3e35840003a30c072246c899592a2fd4
Alexander Litvan <allitvan78@gmail.com>
2024-09-23 14:02:42 -0400

    adding protected routes

--- frontend/src/Routes/Routes.tsx
+++ frontend/src/Routes/Routes.tsx
@@ -1,37 +1,50 @@
 import { createBrowserRouter } from "react-router-dom";
 import App from "../App";
 import HomePage from "../Pages/HomePage/HomePage";
-import SearchPage from "../Pages/SearchPage/SearchPage";
 import CompanyPage from "../Pages/CompanyPage/CompanyPage";
-import IncomeStatement from "../Components/IncomeStatement/IncomeStatement";
+import SearchPage from "../Pages/SearchPage/SearchPage";
 import CompanyProfile from "../Components/CompanyProfile/CompanyProfile";
-import DesignPage from "../Pages/DesignPage/DesignPage";
+import IncomeStatement from "../Components/IncomeStatement/IncomeStatement";
+import DesignGuide from "../Pages/DesignPage/DesignPage";
 import BalanceSheet from "../Components/BalanceSheet/BalanceSheet";
-import CashflowStatement from "../Components/CashFlowStatement/CashFlowStatement";
 import HistoricalDividend from "../Components/HistoricalDividend/HistoricalDividend";
+import CashflowStatement from "../Components/CashFlowStatement/CashFlowStatement";
 import LoginPage from "../Pages/LoginPage/LoginPage";
 import RegisterPage from "../Pages/RegisterPage/RegisterPage";
+import ProtectedRoute from "./ProtectedRoutes";
 
 export const router = createBrowserRouter([
-    {
-        path: "/",
-        element: <App/>,
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      { path: "", element: <HomePage /> },
+      { path: "login", element: <LoginPage /> },
+      { path: "register", element: <RegisterPage /> },
+      {
+        path: "search",
+        element: (
+          <ProtectedRoute>
+            <SearchPage />
+          </ProtectedRoute>
+        ),
+      },
+      { path: "design-guide", element: <DesignGuide /> },
+      {
+        path: "company/:ticker",
+        element: (
+          <ProtectedRoute>
+            <CompanyPage />
+          </ProtectedRoute>
+        ),
         children: [
-            {path: "", element: <HomePage/>},
-            {path: "login", element: <LoginPage/>},
-            {path: "register", element: <RegisterPage/>},
-            {path: "search", element: <SearchPage/>},
-            {path: "design-guide", element: <DesignPage/>},
-            {path: "company/:ticker", 
-                element: <CompanyPage/>,
-               children:[
-                {path: "company-profile", element: <CompanyProfile/>},
-                {path: "income-statement", element: <IncomeStatement/>},
-                {path: "balance-sheet", element: <BalanceSheet/>},
-                {path: "cashflow-statement", element: <CashflowStatement/>},
-                {path: "historical-dividend", element: <HistoricalDividend/>}
-               ] 
-            },
-        ]
-    }
-])
+          { path: "company-profile", element: <CompanyProfile /> },
+          { path: "income-statement", element: <IncomeStatement /> },
+          { path: "balance-sheet", element: <BalanceSheet /> },
+          { path: "cashflow-statement", element: <CashflowStatement /> },
+          { path: "historical-dividend", element: <HistoricalDividend /> },
+        ],
+      },
+    ],
+  },
+]);
