abf482a3ad4525d060b564b9d9620aa320e17803
Alexander Litvan <allitvan78@gmail.com>
2024-09-24 10:44:43 -0400

    adding save portfolio to db

--- frontend/src/Pages/SearchPage/SearchPage.tsx
+++ frontend/src/Pages/SearchPage/SearchPage.tsx
@@ -1,69 +1,100 @@
-import React, { ChangeEvent, SyntheticEvent, useState } from 'react'
-import { CompanySearch } from '../../company';
-import { searchCompanies } from '../../api';
-import Search from '../../Components/Search/Search';
-import Navbar from '../../Components/Navbar/Navbar';
-import ListPortfolio from '../../Components/Portfolio/ListPortfolio/ListPortfolio';
-import CardList from '../../Components/CardList/CardList';
-interface Props{}
+import React, { useState, ChangeEvent, SyntheticEvent, useEffect } from "react";
+import Navbar from "../../Components/Navbar/Navbar";
+import { CompanySearch } from "../../company";
+import { searchCompanies } from "../../api";
+import Search from "../../Components/Search/Search";
+import ListPortfolio from "../../Components/Portfolio/ListPortfolio/ListPortfolio";
+import CardList from "../../Components/CardList/CardList";
+import { PortfolioGet } from "../../Models/Protfolio";
+import {
+  portfolioAddAPI,
+  portfolioDeleteAPI,
+  portfolioGetAPI,
+} from "../../Services/PortfolioService";
+import { toast } from "react-toastify";
+
+interface Props {}
 
 const SearchPage = (props: Props) => {
-    const [search, setSearch] = useState<string>("");
-    const [portfolioValues, setPortfolioValues] = useState<string[]>([]);
-    const [searchResult, setSearchResult] = useState<CompanySearch[]>([]);
-    const [serverError, setServerError] = useState<string>("");
-    
-    const handleSearchChange = (e: ChangeEvent<HTMLInputElement>) => {
-      setSearch(e.target.value);
-      
-    }
-    
-    const onPortfolioCreate = (e: any) => {
-      e.preventDefault();
-      const exists = portfolioValues.find((value) => value === e.target[0].value);
-      if(exists){
-        return;
-      }
-      const updatedPortfolio = [...portfolioValues, e.target[0].value];
-      setPortfolioValues(updatedPortfolio);
-    };
-  
-    const onPortfolioDelete = (e: any) => {
-      e.preventDefault();
-      const removed = portfolioValues.filter((value) => {
-        return value !== e.target[0].value;
-      });
-      setPortfolioValues(removed);
-    };
-  
-    const onSearchSubmit = async (e: SyntheticEvent) => {
-      e.preventDefault();
-      const result = await searchCompanies(search);
-      if(typeof result === "string"){
-        setServerError(result);
-      } else if (Array.isArray(result.data)){
-          setSearchResult(result.data);
-      }
-      console.log(searchResult);
-    };
-    return (
-    <div className="App">
-    <Search 
-        onSearchSubmit={onSearchSubmit} 
-        search={search} 
-        handleSearchChange={handleSearchChange}
-    />
-    <ListPortfolio 
-        portfolioValues={portfolioValues} 
-        onPortfolioDelete={onPortfolioDelete}
-    />
-    <CardList 
-        searchResults={searchResult} 
-        onPortfolioCreate={onPortfolioCreate}
-    />
-    {serverError && <h1>{serverError}</h1>}
-  </div>
-  )
-}
+  const [search, setSearch] = useState<string>("");
+  const [portfolioValues, setPortfolioValues] = useState<PortfolioGet[] | null>(
+    []
+  );
+  const [searchResult, setSearchResult] = useState<CompanySearch[]>([]);
+  const [serverError, setServerError] = useState<string | null>(null);
 
-export default SearchPage
+  useEffect(() => {
+    getPortfolio();
+  }, []);
+
+  const handleSearchChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setSearch(e.target.value);
+  };
+
+  const getPortfolio = () => {
+    portfolioGetAPI()
+      .then((res) => {
+        if (res?.data) {
+          setPortfolioValues(res?.data);
+        }
+      })
+      .catch((e) => {
+        setPortfolioValues(null);
+      });
+  };
+
+  const onPortfolioCreate = (e: any) => {
+    e.preventDefault();
+    portfolioAddAPI(e.target[0].value)
+      .then((res) => {
+        if (res?.status === 204) {
+          toast.success("Stock added to portfolio!");
+          getPortfolio();
+        }
+      })
+      .catch((e) => {
+        toast.warning("Could not add stock to portfolio!");
+      });
+  };
+
+  const onPortfolioDelete = (e: any) => {
+    e.preventDefault();
+    portfolioDeleteAPI(e.target[0].value).then((res) => {
+      if (res?.status == 200) {
+        toast.success("Stock deleted from portfolio!");
+        getPortfolio();
+      }
+    });
+  };
+
+  const onSearchSubmit = async (e: SyntheticEvent) => {
+    e.preventDefault();
+    const result = await searchCompanies(search);
+    //setServerError(result.data);
+    if (typeof result === "string") {
+      setServerError(result);
+    } else if (Array.isArray(result.data)) {
+      setSearchResult(result.data);
+    }
+  };
+  return (
+    <>
+      <Search
+        onSearchSubmit={onSearchSubmit}
+        search={search}
+        handleSearchChange={handleSearchChange}
+      />
+      <ListPortfolio
+        portfolioValues={portfolioValues!}
+        onPortfolioDelete={onPortfolioDelete}
+      />
+      <CardList
+        searchResults={searchResult}
+        onPortfolioCreate={onPortfolioCreate}
+      />
+      {serverError && <div>Unable to connect to API</div>}
+    </>
+  );
+};
+
+export default SearchPage;
--- frontend/src/Services/PortfolioService.tsx
+++ frontend/src/Services/PortfolioService.tsx
@@ -1,11 +1,32 @@
-import React from 'react'
+import axios from "axios";
+import { PortfolioGet, PortfolioPost } from "../Models/Protfolio";
+import { handleError } from "../Helpers/ErrorHandler";
 
-const PortfolioService = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+const api = "http://localhost:5278/api/portfolio/";
 
-export default PortfolioService
+export const portfolioAddAPI = async (symbol: string) => {
+  try {
+    const data = await axios.post<PortfolioPost>(api + `?symbol=${symbol}`);
+    return data;
+  } catch (error) {
+    handleError(error);
+  }
+};
+
+export const portfolioDeleteAPI = async (symbol: string) => {
+  try {
+    const data = await axios.delete<PortfolioPost>(api + `?symbol=${symbol}`);
+    return data;
+  } catch (error) {
+    handleError(error);
+  }
+};
+
+export const portfolioGetAPI = async () => {
+  try {
+    const data = await axios.get<PortfolioGet[]>(api);
+    return data;
+  } catch (error) {
+    handleError(error);
+  }
+};
