5ae56fca79b9154647468c8e248eb4205e57f54b
Alexander Litvan <allitvan78@gmail.com>
2024-09-24 09:43:51 -0400

    adding comments

--- frontend/src/Services/CommentService.tsx
+++ frontend/src/Services/CommentService.tsx
@@ -1,11 +1,30 @@
-import React from 'react'
+import axios from "axios";
+import { CommentGet, CommentPost } from "../Models/Comments";
+import { handleError } from "../Helpers/ErrorHandler";
 
-const CommentService = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+const api = "http://localhost:5167/api/comment/";
 
-export default CommentService
+export const commentPostAPI = async (
+  title: string,
+  content: string,
+  symbol: string
+) => {
+  try {
+    const data = await axios.post<CommentPost>(api + `${symbol}`, {
+      title: title,
+      content: content,
+    });
+    return data;
+  } catch (error) {
+    handleError(error);
+  }
+};
+
+export const commentGetAPI = async (symbol: string) => {
+  try {
+    const data = await axios.get<CommentGet[]>(api + `?Symbol=${symbol}`);
+    return data;
+  } catch (error) {
+    handleError(error);
+  }
+};
